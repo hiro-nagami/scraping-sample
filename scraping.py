45cc732842c24c0f677c975ed0a77a26f2732675
HIROMASA NAGAMINE <hiro.nagami@gmail.com>
2018-10-09 18:10:20 +0900

    Add scraping target

--- scraping.py
+++ scraping.py
@@ -1,9 +1,18 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-html = urlopen("http://pythonscraping.com/blog/second-edition-changes")
-bsObj = BeautifulSoup(html, "html.parser")
+try:
+    html = urlopen("http://pythonscraping.comi/blog/second-edition-changes")
+    bsObj = BeautifulSoup(html, "html.parser")
 
-print(bsObj.h1)
-print(bsObj.h2)
+    print(bsObj.h1)
+    print(bsObj.h2)
+except HTTPError as e:
+    print (e)
+except URLError as e:
+    print ("The server could not found.")
+else:
+    print ("It worked.")
+
+
 
